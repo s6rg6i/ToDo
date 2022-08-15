349fba5cfde8c166921472b895e1b403dba4659b
s6rg6i <90612165+s6rg6i@users.noreply.github.com>
2022-08-15 22:26:30 +0300

    created todoapp (#3)
    Co-authored-by: Sergey Karnaukhov <s_karn_v@mail.ru>

---
+++ backend/todoapp/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework.serializers import ModelSerializer
+from todoapp.models import Project, ToDo
+
+class ProjectModelSerializer(ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+class ToDoModelSerializer(ModelSerializer):
+    class Meta:
+        model = ToDo
+        fields = '__all__'
