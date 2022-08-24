88f1e6a762718e9f83bdee038ed8020894b6fc36
Sergey Karnaukhov <s_karn_v@mail.ru>
2022-08-24 23:52:42 +0300

    Authentication

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,6 @@
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
+from rest_framework.permissions import DjangoModelPermissions
 
 from users.models import CustomUser
 from users.serializers import CustomUserModelSerializer
@@ -10,4 +11,4 @@ from users.serializers import CustomUserModelSerializer
 class CustomUserModelViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericViewSet):
     serializer_class = CustomUserModelSerializer
     queryset = CustomUser.objects.all()
-      
+    permission_classes = [DjangoModelPermissions]
